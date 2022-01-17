45d739da81c68224c948e01d926db5630ec36457
Ganiga <234801@NTTDATA.COM>
2022-01-17 12:18:57 +0530

    Modified the file

--- Q1.java
+++ Q1.java
@@ -4,21 +4,7 @@
 		d.replaceRepeating("Rachana", '*');	
 	 }
 	 
-	 public void replaceRepeating(String s, char c) {
-		 int count=0;
-		 for(int i=0;i<s.length(); i++){
-			 count=0;
-			 for(int j=0;j<s.length(); j++) {
-				 if(s.charAt(i) == s.charAt(j)){
-					 count++;
-				 }
-				 if(count>1) {
-					 s = s.replace(s.charAt(i), c);
-				 }
-			 }
-		
-		}
-		System.out.println(s);
+	
 	 }
 	  
  }
